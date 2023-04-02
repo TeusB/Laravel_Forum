3cffc52397b0d11a803aa95aff6fd5740204a5e8
Frits Notenboom <teus.brom@ictmbo.nl>
2023-04-02 18:22:29 +0200

    link fix

--- resources/js/axios.js
+++ resources/js/axios.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://fritsnotenboom.nl/',
+    baseURL: 'https://fritsnotenboom.nl:8000/api/',
     timeout: 5000
-    
 })
 
 export default instance
